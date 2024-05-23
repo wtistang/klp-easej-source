e11406d0a9eaf9734d7339332c3cac6c29bf4afb
Alasdair Nottingham <alasdair.nottingham@gmail.com>
2024-05-23 07:59:11 -0400

    add tests

---
+++ src/test/java/com/acme/modres/test/TestSuite01.java
@@ -0,0 +1,12 @@
+package com.acme.modres.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    UnitTest.class
+})
+public class TestSuite01 {
+}
